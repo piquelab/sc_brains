facae50950a47183df0a19d176407b96d0076ecb
JulongWei <weijulong128@sina.com>
2022-01-26 11:21:11 -0500

    demux for Mash

---
+++ Analysis/Mash2021-11-28/1_demux2.R
@@ -0,0 +1,57 @@
+######################################
+### demux2 for downstream analysis ###
+######################################
+library(Matrix)
+library(tidyverse)
+library(parallel)
+library(data.table)
+
+
+
+rm(list=ls())
+
+outdir <- "./1_demux2_v2_output/"
+if (!file.exists(outdir)) dir.create(outdir, showWarnings=F)
+
+#################################
+### 1. folders of counts data ###
+#################################
+basefolder <- "../../counts_brain_2021Nov_Mash/demuxlet2/demuxlet/"
+demuxfn <- list.files(basefolder,pattern="*.out.best")
+
+# all the libraries from the membrane placenta 
+expNames <- gsub(".out.best", "", demuxfn) 
+## remove bad emulsions
+
+############################
+### 2, read demuxlet data ###
+############################
+
+# new column "NEW_BARCODE" was added to the data frame associated to each experiment (experiment name+barcode) "-1" was removed from end of barcode
+
+demux <- mclapply(expNames,function(ii){
+  cat("#Loading ", ii, "\n")
+  fn <- paste0(basefolder, ii,".out.best")
+  dd <- data.frame(fread(fn,header=T))
+  dd <- dd%>%mutate(NEW_BARCODE=paste0(ii,"_", gsub("-1","",BARCODE)),EXP=ii)
+},mc.cores=10)
+
+
+# merging all the experiments  (12 experiments)
+demux <- do.call(rbind,demux)
+###
+
+## saving the final demux file obtained from all experiments into "1_demux_New.ALL.rds" file
+### output
+opfn <- paste(outdir, "1_demux_New.ALL.rds", sep="")
+write_rds(demux, opfn)
+
+## DROPLET.TYPE: "SNG" 
+opfn <- paste(outdir, "1_demux_New.SNG.rds", sep="") 
+demux2 <- demux %>% filter(DROPLET.TYPE=="SNG")
+write_rds(demux2, opfn)
+
+
+
+
+
